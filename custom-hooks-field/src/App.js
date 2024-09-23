1e1460a8027651c1ab94a1b04f5d21adc7299f34
NhiDinh <thaonhi6991@gmail.com>
2024-09-23 10:25:58 +0300

    first commit

---
+++ custom-hooks-field/src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import AppWithCustomHook from './AppWithCustomHook';
+
+const App = () => {
+  return (
+    <div>
+      <AppWithCustomHook/>
+    </div>
+  );
+};
+
+export default App;
